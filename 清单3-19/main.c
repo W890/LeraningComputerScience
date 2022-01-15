21458884bdaf5ba8a80a6a97f4de7eded7b3faaa
YK W <2848056890@qq.com>
2022-01-15 18:25:13 +0800

    test

--- 清单3-19/main.c
+++ 清单3-19/main.c
@@ -8,9 +8,9 @@ int main()
 	scanf("%d", &no);
 
 	switch (no % 3) {
-	case 0: puts("该整数能被3整除。");
-	case 1: puts("该数除以3的余数是1。");
-	case 2: puts("该数除以3的余数是2。");
+	case 0: puts("该整数能被3整除。");       break;
+	case 1: puts("该数除以3的余数是1。");		break;
+	case 2: puts("该数除以3的余数是2。");		break;
 	}
 
 	return 0;
