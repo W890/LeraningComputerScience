503700bd851ef589242ffc445d25a484e7fa9eca
YK W <2848056890@qq.com>
2022-02-04 09:10:39 +0800

    5-2

--- 练习5-2/main.c
+++ 练习5-2/main.c
@@ -5,8 +5,8 @@ int main ()
 	int v[5];
 
 	for (i = 0; i < 5; i++)
-		v[i] = i + 1;
-	for (i = 4; i >= 0; i--)
+		v[i] = 5 - i;
+	for (i = 0; i < 5; i++)
 		printf("v[%d] = %d\n", i, v[i]);
 
 	return 0;
