890224c84460c10976fd04352dc04a5c78cce321
YK W <2848056890@qq.com>
2022-02-21 22:35:06 +0800

    8-3

--- 练习8-3/main.c
+++ 练习8-3/main.c
@@ -1,10 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#define swap(int, a, b)  int x; x = a; a = b; b = x;
+#define swap(type, a, b)  {type x = a; a = b; b = x;}
 int main()
 {
 	int a, b;
-
 	puts("请输入两个整数：");
 	scanf("%d %d", &a, &b);
 	swap(int, a, b);
