ac6adee5f77069d33f96c024bb9fc3925b86b625
YK W <2848056890@qq.com>
2022-03-03 23:48:55 +0800

    9-5

--- 练习9-5/main.c
+++ 练习9-5/main.c
@@ -3,11 +3,9 @@
 #include<string.h>
 int str_char(const char s[], int c) {
     int i;
-    for (i = 0; i < strlen(s); i++) {
-        if (s[i] == 'c') {
-            break;
-        }
-    }
+    for (i = 0; i < strlen(s); i++) 
+        if (s[i] == 'c') 
+    
     return i;
 }
 int main()
@@ -17,6 +15,10 @@ int main()
     printf("请输入字符串: ");
     scanf("%s", ch);
     i = str_char(ch, 'c');
+    if ('c')
     printf("字符c的下标值为%d", i);
+    else 
+    printf("-1");
+
     return 0;
 }
