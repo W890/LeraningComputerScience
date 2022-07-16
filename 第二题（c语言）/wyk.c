ca3c158ef0468af35c6fac6f44dce1d8f4a95da0
YK W <2848056890@qq.com>
2022-07-16 15:42:19 +0800

    “第二题”

--- 第二题（c语言）/wyk.c
+++ 第二题（c语言）/wyk.c
@@ -1,21 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
 
-int del_digit(char* str)
+int is_full_alpha_sentence(char str[], int n)
 {
-    while (*str) {
-        if (*str >= 'a' && *str <= 'z') {
-            return 1;
-        } else {
+    int a[26] = { 0 }, j = 0,i;
+
+    for (i = 0; i < n; i++) {    
+        a[str[i] - 'a'] = 1;             
+    }  
+    for (i = 0; i < 26; i++) {
+        if (a[i] == 0)
             return 0;
-        }
     }
+    return 1;
 }
 
 int main()
 {
-    char* arr = "sentence";
+    char* sentence = "abcdefghijklmnopqrstuvwxyzadpapsfdp";
 
-    int ab = del_digit(arr);
+    int ab = is_full_alpha_sentence(sentence, strlen(sentence));
     if (ab)
         printf("true");
     else {
